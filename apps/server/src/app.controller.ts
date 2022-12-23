ab95a770b27c350d82c254f9ea2877962e1bb413
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-12-23 15:14:29 +0700

    try deploy

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get, Redirect } from "@nestjs/common";
 
+import { Permission } from "./auth/auth.decorator";
+
 @Controller()
+@Permission("Public")
 export class AppController {
   @Get()
   @Redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
